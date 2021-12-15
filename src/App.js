44c9d1f74903418c89666179503c57d3cc673bf2
Senshi <maxlxrd59@hotmail.com>
2021-12-15 21:09:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Nav from './components/Nav';
+import Event from './components/Event';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Event />
     </div>
   );
 }
